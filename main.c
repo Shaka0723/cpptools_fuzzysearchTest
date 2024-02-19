a0a03eb0f1f71485d50bb6aaa0e1d77a4f9b725f
shyang <shyang@jabra.com>
2024-02-19 14:11:06 +0800

    test

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int thisisvarname;
+int thisisafoovarname;
+int ThisIsAFooVarName;
+int ThisIsAFooVarName2;
+int ThisIsAFooVarName22;
+int ThisIsAFooVarName232;
+
+int UsbAudioSendSpeakerVolumeGain;
+int usbaudiosendspeakervolumegain;
+
+int main()
+{
+  return 0;
+}
+
+int doSomethingMoreComplicated ;
+int do_something_more_complicated ;
